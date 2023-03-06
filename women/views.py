9e8a9828d05341af32a3502d489e820e22a179e4
Evgeniy <pron3000@list.ru>
2023-03-06 18:34:25 +0600

    Python django upload

---
+++ women/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request): #HttpRequest
+    return HttpResponse("Страница приложения women.")
+
+def categories(request, catid):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
