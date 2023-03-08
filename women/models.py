5ee9fa790f30803383c4b21f6f3f7ddafc3b3cee
Evgeniy <pron3000@list.ru>
2023-03-08 20:51:17 +0600

    URLs for menu and posts

--- women/models.py
+++ women/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
 
@@ -12,3 +14,6 @@ class Women(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
