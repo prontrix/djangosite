c1bc3dbe6ffb7bc4c44308ac563b437c4c5e1e2a
Evgeniy <pron3000@list.ru>
2023-03-10 12:57:32 +0600

    Customizing admin panel. Added photo in posts

--- women/admin.py
+++ women/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class WomenAdmin(admin.ModelAdmin): # вспомогательный класс, чтобы в админке список статей был с этими полями
+    list_display = ('id', 'title', 'cat', 'photo', 'time_create', 'is_published')
+    list_display_links = ('id', 'title') # какие поля будут ссылками
+    search_fields = ('title', 'content') # по каким полям делать поиск
+    list_editable = ('is_published',) # какие поля можно редактировать прямо в списке
+    list_filter = ('is_published', 'time_create') # фильтр справа: по этим полям можно фильтровать статьи в списке
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',) # нужна запятая, т.к. всего один элемент, а это должен быть кортеж
+
+
+admin.site.register(Women, WomenAdmin)
+admin.site.register(Category, CategoryAdmin)
--- women/models.py
+++ women/models.py
@@ -5,13 +5,13 @@ from django.urls import reverse
 # Create your models here.
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+    title = models.CharField(max_length=255, verbose_name="Заголовок")
+    content = models.TextField(blank=True, verbose_name="Текст статьи")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name="Категория")
 
     def __str__(self):
         return self.title
@@ -19,11 +19,21 @@ class Women(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+    class Meta: # все изменения только для админки
+        verbose_name = 'Известные женщины' # отображаемое имя вместо Womens
+        verbose_name_plural = 'Известные женщины' # множественное число в списке слева
+        ordering = ['-time_create', 'title'] # сортировка статей (работает и в админке, и на сайте)
+
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Категория")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
