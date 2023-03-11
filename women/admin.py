e85d4588f79ba61f0b4ff473f907a9e84bf20851
Evgeniy <pron3000@list.ru>
2023-03-11 12:21:11 +0600

    Changed post's and category's url from id to slug

--- women/admin.py
+++ women/admin.py
@@ -8,12 +8,13 @@ class WomenAdmin(admin.ModelAdmin): # вспомогательный класс,
     search_fields = ('title', 'content') # по каким полям делать поиск
     list_editable = ('is_published',) # какие поля можно редактировать прямо в списке
     list_filter = ('is_published', 'time_create') # фильтр справа: по этим полям можно фильтровать статьи в списке
+    prepopulated_fields = {"slug": ("title",)}  # автоматическая генерация slug в админке
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id', 'name')
     search_fields = ('name',) # нужна запятая, т.к. всего один элемент, а это должен быть кортеж
-
+    prepopulated_fields = {"slug": ("name",)}
 
 admin.site.register(Women, WomenAdmin)
 admin.site.register(Category, CategoryAdmin)
