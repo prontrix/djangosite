90f7484d72ae4a0e8385b2f8eb083822cceb1eac
Evgeniy <pron3000@list.ru>
2023-03-13 14:33:57 +0600

    Changed adding post form to use class Women. Added user field validation

--- women/forms.py
+++ women/forms.py
@@ -1,9 +1,27 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import *
 
-class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255, label="Заголовок", widget=forms.TextInput(attrs={'class': 'form-input'}))
-    slug = forms.SlugField(max_length=255, label="URL")
-    content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label="Контент")
-    is_published = forms.BooleanField(label="Публикация", required=False, initial=True)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all(), label="Категория", empty_label="Категория не выбрана")
+class AddPostForm(forms.ModelForm):
+    # добавляем конструктор, если хотим переопределить поля, например, выпадающий список
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cat'].empty_label = "Категория не выбрана"
+
+    class Meta:
+        model = Women # взаимодействие с оновной моделью
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat'] # какие поля будут в форме, можно __all__, чтобы были все
+        # стили оформления отдельных полей
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
+        }
+
+    # clean_* - пользовательский валидатор для конкретного поля формы
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 200:
+            raise ValidationError('Длина превышает 200 символов')
+
+        return title
