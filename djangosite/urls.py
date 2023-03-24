16fb578ba0ab82902d25bd3e56b2b233bf0e4509
Evgeniy <pron3000@list.ru>
2023-03-24 11:29:48 +0600

    Added feedback form with captcha

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -24,6 +24,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('captcha/', include('captcha.urls')),
     path('', include('women.urls')),
 ]
 
